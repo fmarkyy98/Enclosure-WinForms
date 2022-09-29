3a3f45d8758dc6adad38ae81dd921e31cec196eb
Márk Fekete <e0vhz5@INF.ELTE.HU>
2022-09-29 13:27:51 +0200

    Model skeleton

--- Enclosure-WinForms/Enclosure-WinForms/Model/EnclosureModel.cs
+++ Enclosure-WinForms/Enclosure-WinForms/Model/EnclosureModel.cs
@@ -6,9 +6,34 @@ using System.Threading.Tasks;
 
 namespace Enclosure_WinForms.Model
 {
-    enum Field
+    enum Player { Red, Blue };
+    enum FieldState { Free, RedPlaced, BluePlaced, RedCaptured, BlueCaptured };
+    enum GameSize { Small = 6, Medium = 8, Large = 10 };
+
+    public struct Scores
+    {
+        public int Red;
+        public int Blue;
+    }
 
     internal class EnclosureModel
     {
+        public EnclosureModel()
+        {
+            initGame(GameSize.Small);
+        }
+
+        public void initGame(GameSize gameSize)
+        {
+            currentPlayer_ = Player.Blue;
+            scores.Blue = scores.Red = 0;
+
+            int size = (int)gameSize;
+            FieldState[,] board_ = new FieldState[size, size];
+        }
+        
+        Player currentPlayer_;
+        Scores scores;
+        FieldState[,] board_; 
     }
 }
