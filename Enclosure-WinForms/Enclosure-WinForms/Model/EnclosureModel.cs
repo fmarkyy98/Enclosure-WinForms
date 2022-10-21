4f2cd11953d71ed33e4ce4ec6bf1ca76e1380a5c
fmarkyy98 <fmarkyy98@gmail.com>
2022-10-21 22:55:00 +0200

    Basic game works

--- Enclosure-WinForms/Enclosure-WinForms/Model/EnclosureModel.cs
+++ Enclosure-WinForms/Enclosure-WinForms/Model/EnclosureModel.cs
@@ -51,13 +51,19 @@ namespace Enclosure_WinForms.Model
             }
             else // (lsatClickPos != null)
             {
-                if (board_[x, y] != FieldState.Free && hasCommonNeighbour(x, y))
+                if (board_[x, y] != FieldState.Free || !hasCommonNeighbour(x, y))
                     return;
 
                 board_[x, y] = (FieldState)currentPlayer_;
+                captureSuroundedFields();
+
                 lastClickPos = null;
                 currentPlayer_ = (Player)((int)currentPlayer_ % 2 + 1);
             }
+
+
+
+            BoardCnahged?.Invoke(this, board_);
         }
 
         private void initGame(GameSize gameSize)
@@ -69,7 +75,7 @@ namespace Enclosure_WinForms.Model
             ScoresCnahged?.Invoke(this, scores);
 
             int size = (int)gameSize;
-            FieldState[,] board_ = new FieldState[size, size];
+            board_ = new FieldState[size, size];
             BoardCnahged?.Invoke(this, board_);
         }
 
@@ -81,11 +87,78 @@ namespace Enclosure_WinForms.Model
             int l_x = lastClickPos.Value.x;
             int l_y = lastClickPos.Value.y;
 
-            bool result = (x-1 == l_x && y   == l_y) ||
-                          (x+1 == l_x && y   == l_y) ||
-                          (x   == l_x && y-1 == l_y) ||
-                          (x   == l_x && y+1 == l_y);
+            bool result = (x - 1 == l_x && y == l_y) ||
+                          (x + 1 == l_x && y == l_y) ||
+                          (x == l_x && y - 1 == l_y) ||
+                          (x == l_x && y + 1 == l_y);
             return result;
+        }
+
+        private void captureSuroundedFields()
+        {
+            int n = BoardSize;
+
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+
+                    try
+                    {
+                        bool[,] checkedFields = new bool[n, n];
+                        Player? capturer = AmISurrounded(i, j, checkedFields);
+                        if (capturer != null)
+                            board_[i, j] = (FieldState)(int)capturer.Value + 2;
+
+                    }
+                    catch (IndexOutOfRangeException e) { }
+                }
+            }
+        }
+
+        Player? AmISurrounded(int x, int y, bool[,] checkedFields)
+        {
+            checkedFields[x, y] = true;
+            Player? capturer = null;
+
+            Player? left = null;
+            if ((int)board_[x - 1, y] == (int)currentPlayer_)
+                left = (Player)board_[x - 1, y];
+            else if (!checkedFields[x - 1, y])
+                left = AmISurrounded(x - 1, y, checkedFields);
+
+
+            Player? right = null;
+            if ((int)board_[x + 1, y] == (int)currentPlayer_)
+                right = (Player)board_[x + 1, y];
+            else if (!checkedFields[x + 1, y])
+                right = AmISurrounded(x + 1, y, checkedFields);
+
+            Player? top = null;
+            if ((int)board_[x, y - 1] == (int)currentPlayer_)
+                top = (Player)board_[x, y - 1];
+            else if (!checkedFields[x, y - 1])
+                top = AmISurrounded(x, y - 1, checkedFields);
+
+            Player? bot = null;
+            if ((int)board_[x, y + 1] == (int)currentPlayer_)
+                bot = (Player)board_[x, y + 1];
+            else if (!checkedFields[x, y + 1])
+                bot = AmISurrounded(x, y + 1, checkedFields);
+
+            if (left == null && right == null && top == null && bot == null)
+                return null;
+
+            capturer = left ?? right ?? top ?? bot;
+
+            foreach (var res in new List<Player?> { left, right, top, bot })
+            {
+                if (res != null && res != capturer)
+                    capturer = null;
+            }
+
+            return capturer;
         }
 
         IDataAccess dataAccess_;
--- Enclosure-WinForms/Enclosure-WinForms/View/GameForm.cs
+++ Enclosure-WinForms/Enclosure-WinForms/View/GameForm.cs
@@ -12,9 +12,38 @@ namespace Enclosure_WinForms
 
             Load += OnLoad;
 
-            smallBtn.Click  += (_,_) => { NewGame(GameSize.Small); };
-            mediumBtn.Click += (_,_) => { NewGame(GameSize.Medium); };
-            largeBtn.Click  += (_,_) => { NewGame(GameSize.Large); };
+            smallBtn.Click += (_, _) => { NewGame(GameSize.Small); };
+            mediumBtn.Click += (_, _) => { NewGame(GameSize.Medium); };
+            largeBtn.Click += (_, _) => { NewGame(GameSize.Large); };
+
+            gameModel_.BoardCnahged += (_, board) =>
+            {
+                int n = model.BoardSize;
+                for (int i = 0; i < n; ++i)
+                {
+                    for (int j = 0; j < n; ++j)
+                    {
+                        switch (board[j, i])
+                        {
+                            case FieldState.Free:
+                                tableLayoutPanel1.Controls[i * n + j].BackColor = Color.Gray;
+                                break;
+                            case FieldState.RedPlaced:
+                                tableLayoutPanel1.Controls[i * n + j].BackColor = Color.Red;
+                                break;
+                            case FieldState.BluePlaced:
+                                tableLayoutPanel1.Controls[i * n + j].BackColor = Color.Blue;
+                                break;
+                            case FieldState.RedCaptured:
+                                tableLayoutPanel1.Controls[i * n + j].BackColor = Color.Pink;
+                                break;
+                            case FieldState.BlueCaptured:
+                                tableLayoutPanel1.Controls[i * n + j].BackColor = Color.LightBlue;
+                                break;
+                        }
+                    }
+                }
+            };
         }
 
         private void OnLoad(object? sender, EventArgs e)
@@ -28,6 +57,17 @@ namespace Enclosure_WinForms
             tableLayoutPanel1.RowCount = (int)gameSize;
             tableLayoutPanel1.ColumnCount = (int)gameSize;
 
+            tableLayoutPanel1.ColumnStyles.Clear();
+            tableLayoutPanel1.RowStyles.Clear();
+            for (int i = 0; i < tableLayoutPanel1.RowCount; i++)
+            {
+                tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            }
+            for (int i = 0; i < tableLayoutPanel1.ColumnCount; i++)
+            {
+                tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            }
+
             int n = gameModel_.BoardSize;
             for (int i = 0; i < n; ++i)
             {
@@ -36,9 +76,16 @@ namespace Enclosure_WinForms
                     Label l = new();
                     l.BackColor = Color.Gray;
                     l.Text = $"({j};{i})";
+                    l.AutoSize = false;
+                    l.Dock = DockStyle.Fill;
                     tableLayoutPanel1.Controls.Add(l);
 
-                    l.Click += (_,_) => { gameModel_.ClickedAt(j, i); };
+                    l.Click += (sender, _) =>
+                    {
+                        int x = tableLayoutPanel1.Controls.IndexOf((Label)sender) % n;
+                        int y = tableLayoutPanel1.Controls.IndexOf((Label)sender) / n;
+                        gameModel_.ClickedAt(x, y);
+                    };
                 }
             }
 
@@ -46,5 +93,6 @@ namespace Enclosure_WinForms
         }
 
         private IEnclosureModel gameModel_;
+
     }
 }
